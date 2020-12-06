38c27760ab3cb6581c1154c309f29a2084cae087
Aethese <59108437+Aethese@users.noreply.github.com>
2020-12-05 23:21:27 -0600

    watching something

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_ready():
-  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=""))
+  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="anime"))
   print("Bot is online!")
 
 @bot.command(aliases=['logout'])
